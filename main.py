dd3fec47e2440644290d87eb58836aea62c1d861
DAN <ddryuchin@gmail.com>
2020-12-28 21:11:30 +0500

    cxdf

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
 
 
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyWidget()
