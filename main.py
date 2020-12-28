38ce9391ee07e22c9adedcca9fa35d5af61737b8
DAN <ddryuchin@gmail.com>
2020-12-28 21:16:07 +0500

    bruh

--- main.py
+++ main.py
@@ -39,7 +39,6 @@ def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
 
 
-
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyWidget()
